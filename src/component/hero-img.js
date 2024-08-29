1c3466b83c77f8f8c51326f6c71b0727de0f6846
morris <akunnemorris@gmail.com>
2024-08-29 18:24:27 +0100

    6th commit

--- src/component/hero-img.js
+++ src/component/hero-img.js
@@ -1,12 +1,13 @@
 import "./hero-img.css";
 import { Link } from "react-router-dom";
-import React from 'react'
+import React from 'react';
+import pic from "./image/IMG_20230829_141346.png"
 
 const Hero = () => {
   return (
     <div className="hero">
         <div className="mask">
-            <img className="into-img" src="" alt="Intro-img"/>
+            <img className="into-img" src={pic} alt="Intro-img"/>
             </div>
             <div className="content">
                 <p>HI, I'M A FREELANCER.</p>
