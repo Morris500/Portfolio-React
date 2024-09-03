cc477b04bc814f9c6e4f9961b4b24fece326e3ac
morris <akunnemorris@gmail.com>
2024-09-03 17:36:51 +0100

    working on the workcard props

--- src/component/work.js
+++ src/component/work.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import './workcard.css';
-import { Link } from "react-router-dom";
+import Workcard from './workcard';
+import ProjectCardData from './workcaddata';
+
 
 const Work = () => {
   return (
     <div className='work-container'>
        <h1 className='project-heading'>projects</h1>
        <div className='project-container'>
-       
+       {ProjectCardData.map((value, index)=> {
+        return(
+            <Workcard 
+            key={index}
+            imgsrc={value.imgsrc}
+            title={value.title}
+            text={value.text}
+            view={value.view}
+            view2={value.view2}
+            />
+        )
+       })}
     
        </div>
     </div>
