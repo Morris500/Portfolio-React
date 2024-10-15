fdaa2bb24dc8d7807a5d3a46e4c43265ccad51ef
morris <akunnemorris@gmail.com>
2024-10-16 02:33:09 +0400

    commit

--- client/src/component/contact.js
+++ client/src/component/contact.js
@@ -1,6 +1,6 @@
 import "./contact.css";
 import React, {useState} from 'react';
-//import axios from "axios";
+import axios from "axios";
 //import { GoogleApis } from "googleapis";
 const a = process.env.REACT_APP_SECRET;
 console.log(a);
@@ -18,23 +18,34 @@ return({...prevalue, [name]: value})
 
   };
   
-  console.log(handelChange);
-  
-  async function submit(params) {
+  //console.log(handelChange);
+
+  async function submit(event) {
     try {
-      //axios.post()
-    } catch (error) {
+      const url = "http://localhost:4000/contact";
+      event.preventDefault();
+      const data = await axios.post(url, handelChange);
+       console.log(data);
+       
+    //   fetch(url, {
+    //     method: 'POST', 
+    //   headers:{'content-Type': 'application/json'},
+    //   body: JSON.stringify(handelChange)
+    // })
       
+      alert('message sent');
+    } catch (error) {
+      alert(`unable to send at the moment please try again ${error}`)
     }
     
   }
   return (
     <div className="form">
         <form >
-            <input type="text" name="Name" onChange={detail} placeholder="Enter Your Name" />
-            <input type="text" name="Email" onChange={detail} placeholder="Enter Your Email" />
-            <input type="text" name="Subject" onChange={detail} placeholder="Subject" />
-            <textarea rows={6} name="Text" onChange={detail} placeholder="Type your message"></textarea>
+            <input type="text" name="Name" value={handelChange.Name} onChange={detail} placeholder="Enter Your Name" />
+            <input type="text" name="Email" value={handelChange.Email} onChange={detail} placeholder="Enter Your Email" />
+            <input type="text" name="Subject" value={handelChange.Subject} onChange={detail} placeholder="Subject" />
+            <textarea rows={6} name="Text" value={handelChange.Text} onChange={detail} placeholder="Type your message"></textarea>
             <button type="submit" onClick={submit} className="btnws">Submit</button>
         </form>
     </div>
--- server/index.js
+++ server/index.js
@@ -6,18 +6,21 @@ import { fileURLToPath } from "url";
 import cors from "cors";
 
 const app = express();
-const port = 3001;
+const port = 4000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "client/build")));
 app.use(bodyParser.urlencoded({extended:true}))
+//client\build\index.html
 app.use(cors({
-    origin: "http://localhost:3000"
-}));
-
-app.get("/", (req, res)=>{
-
-})
+         origin: "http://localhost:3000"
+     }));
+// {
+//     origin: "http://localhost:3000"
+// }
+ app.get("/", (req, res)=>{
+res.send("helloe world")
+ })
 
 app.get("*", (req,res)=> {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
@@ -26,7 +29,7 @@ app.get("*", (req,res)=> {
 app.post("/contact", (req, res)=>{
  const {Name, Email, Subject, Text} = req.body;
  console.log(Name, Email);
- res.redirect("http://localhost:3000/contact")
+ //res.redirect("http://localhost:3000/contact")
  
 });
 
